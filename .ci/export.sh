4fbdcc2e79f11a8fbbd0d213c1e789f67276b59d
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-04-28 11:21:21 +0530

    change release key decryption path

--- .ci/export.sh
+++ .ci/export.sh
@@ -3,6 +3,6 @@
 export NDK_CCACHE=$(which ccache)
 
 if [ ! -z "${ANDROID_KEYSTORE_B64}" ]; then
-    export ANDROID_KEYSTORE_FILE="${GITHUB_WORKSPACE}/ks.jks"
+    export ANDROID_KEYSTORE_FILE="${GITHUB_WORKSPACE}/src/android/app/ks.jks"
     base64 --decode <<< "${ANDROID_KEYSTORE_B64}" > "${ANDROID_KEYSTORE_FILE}"
 fi
