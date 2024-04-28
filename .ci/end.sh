705b06868a9f21b8bfc05f8d7bff2f4a8f742d59
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-04-28 11:41:41 +0530

    fix ``end.sh``

--- .ci/end.sh
+++ .ci/end.sh
@@ -1,5 +1,6 @@
 ccache -s -v
 
 if [ ! -z "${ANDROID_KEYSTORE_B64}" ]; then
-    rm "${ANDROID_KEYSTORE_FILE}"
+    chmod +x ${GITHUB_WORKSPACE}/src/android/app/ks.jks
+    rm "${GITHUB_WORKSPACE}/src/android/app/ks.jks"
 fi
