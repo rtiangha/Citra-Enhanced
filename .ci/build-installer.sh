4ddada1cb6f4d8a5fe1c813b164f320c44843580
kleidis <167202775+kleidis@users.noreply.github.com>
2024-05-04 20:30:49 +0200

    Add installer workflow build files

---
+++ .ci/build-installer.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -ex
+
+# Make the build dir
+
+mkdir -p $GITHUB_WORKSPACE/build/enhanced-installer
+
+# Go to build dir
+
+cd dist/installer
+
+# Generate Image Data
+
+
+python imagedata.py
+
+# Check if the file was made
+
+FILE=image_base64.py  
+if [ -f $FILE ]; then
+   echo "File $FILE exists."
+else
+   echo "File $FILE does not exist."
+fi
+
+# Run PyInstaller to build the applications
+
+pyinstaller --onefile --windowed --distpath $GITHUB_WORKSPACE/build/enhanced-installer --icon=lemonade.ico --add-data "lemonade.ico;." installer.py
+
+echo $GITHUB_WORKSPACE
+cd $GITHUB_WORKSPACE
