d7292c5cb9f7e9ac0e5670e7ccf9f1605332c4f7
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-05-05 11:20:19 +0530

    missing imports

--- src/android/app/src/main/java/org/citra/citra_emu/adapters/GameAdapter.kt
+++ src/android/app/src/main/java/org/citra/citra_emu/adapters/GameAdapter.kt
@@ -10,6 +10,8 @@ import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.content.Context
+import android.widget.TextView
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
