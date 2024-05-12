f14903aaa75fa3e9c2a4eaf41548dfd734d91021
Gamer64ytb <76565986+Gamer64ytb@users.noreply.github.com>
2024-05-12 11:54:27 +0200

    Always use upscaling hack
    It just fixes issues, so no point about set it as a option. We also have Skip Texture Copy hack

--- src/common/settings.h
+++ src/common/settings.h
@@ -547,7 +547,6 @@ struct Values {
     SwitchableSetting<bool> skip_texture_copy{false, "skip_texture_copy"};
     SwitchableSetting<bool> core_downcount_hack{false, "core_downcount_hack"};
     SwitchableSetting<bool> priority_boost{true, "priority_boost"};
-    SwitchableSetting<bool> upscaling_hack{true, "upscaling_hack"};
     // OpenGL Hack
     SwitchableSetting<bool> gl_stream_buffer_hack{true, "gl_stream_buffer_hack"};
 
