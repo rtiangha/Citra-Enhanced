e2cfaf5764821a760e30399a48ddba2f156524c0
kleidis <167202775+kleidis@users.noreply.github.com>
2024-05-27 17:52:08 +0200

    Remove old installer
    You should use https://github.com/kleidis/Troppical

--- dist/installer/imagedata.py
+++
@@ -1,7 +0,0 @@
-import base64
-
-with open("Citra-Enhanced.png", "rb") as image_file:
-    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
-
-with open("image_base64.py", "w") as text_file:
-    text_file.write(f"image_data = '{encoded_string}'")
